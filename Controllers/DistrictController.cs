04903783d6545402fdd6529b0ad6e420f2f688f5
u <urio@kkl.org.il>
2021-08-12 10:40:20 +0300

    aa

--- Controllers/DistrictController.cs
+++ Controllers/DistrictController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,12 +7,14 @@ namespace YaaranutGisApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [EnableCors("CorsAll")]
     public class DistrictController : BaseController
     {
         public DistrictController(YaaranutGisApi.IAppSettings appSettings, IGisApiHelper GisApiHelper) : base(appSettings, GisApiHelper) { }
 
         [HttpGet]
         [Route("GetDistricts")]
+        [EnableCors("CorsAll")]
         public async Task<ActionResult<IEnumerable<GisDistrictModel.DistrictModel>>> GetDistricts()
         {
             IList<GisDistrictModel.DistrictModel> districts = new List<GisDistrictModel.DistrictModel>();
