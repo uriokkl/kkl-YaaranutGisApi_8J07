dafa0fbcffeef7743cd522d4619ef1287befd9f0
u <urio@kkl.org.il>
2021-10-25 14:44:53 +0300

    11111

--- Controllers/ForestController.cs
+++ Controllers/ForestController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -69,76 +68,4 @@ namespace YaaranutGisApi.Controllers
             public string FOR_Name { get; set; }
         }
     }
-=======
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace YaaranutGisApi.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    [EnableCors("CorsAll")]
-    public class ForestController : BaseController
-    {
-        public ForestController(YaaranutGisApi.IAppSettings appSettings, IGisApiHelper GisApiHelper) : base(appSettings, GisApiHelper) { }
-
-        [HttpGet]
-        [Route("GetForests")]
-        [EnableCors("CorsAll")]
-        public async Task<ActionResult<IEnumerable<ForestModel>>> GetForests()
-        {
-            var reqparmForest = new System.Collections.Specialized.NameValueCollection
-                {
-                    {"where", "1=1" },
-                    {"outFields", YaaranutGisApi.GisApiHelper.GetModelFields(typeof( ForestModel))},
-                    {"returnGeometry", "false"},
-                    {"returnExceededLimitFeatures", "true"},
-                    {"token", this.GisApiHelper.GetToken()},
-                    {"f", "json"},
-                    {"geometryType","esriGeometryPoint"},
-                };
-
-            var Gisfeatures = this.GisApiHelper.GetFeatures<ForestModel>("JNFILForest",0, reqparmForest);
-            if (Gisfeatures.GisAttributes.error == null)
-            {
-                return Ok(Gisfeatures.Features);
-            }
-            else
-            {
-                return StatusCode(500, Gisfeatures.GisAttributes.error.message + " " + Gisfeatures.GisAttributes.error.details[0] + " where:" + reqparmForest.GetValues("where")[0] + " Fields:" + reqparmForest.GetValues("outFields")[0]);
-            }
-        }
-    }
-
-    public class ForestModel
-    {
-        public int? FID { get; set; }
-        public int? District_C { get; set; }
-        public int Region_Cod { get; set; }
-        public int FOR_Num { get; set; }
-        public string FOR_Name { get; set; }
-    }
-
-    public class GisForestModel1111 : GisModel
-    {
-        public Features[] features { get; set; }
-        public class Features
-        {
-            public Attributes attributes { get; set; }
-        }
-        public class Attributes : ForestModel
-        {
-        }
-        public class ForestModel
-        {
-            public int? FID { get; set; }
-            public int? District_C { get; set; }
-            public int Region_Cod { get; set; }
-            public int FOR_Num { get; set; }
-            public string FOR_Name { get; set; }
-        }
-    }
->>>>>>> d1647bf94dd54e593376df04d5bfde8bd46326bf
 }
--- Controllers/RegionController.cs
+++ Controllers/RegionController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -67,74 +66,4 @@ namespace YaaranutGisApi.Controllers
             public string ezor_name { get; set; }
         }
     }
-=======
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace YaaranutGisApi.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    [EnableCors("CorsAll")]
-    public class RegionController : BaseController
-    {
-        public RegionController(YaaranutGisApi.IAppSettings appSettings, IGisApiHelper GisApiHelper) : base(appSettings, GisApiHelper) { }
-
-        [HttpGet]
-        [Route("GetRegions")]
-        [EnableCors("CorsAll")]
-        public async Task<ActionResult<IEnumerable<RegionModel>>> GetRegions()
-        {
-            var reqparmForest = new System.Collections.Specialized.NameValueCollection
-                {
-                    {"where", "1=1" },
-                    {"outFields", YaaranutGisApi.GisApiHelper.GetModelFields(typeof( RegionModel))},
-                    {"returnGeometry", "false"},
-                    {"returnExceededLimitFeatures", "true"},
-                    {"token", this.GisApiHelper.GetToken()},
-                    {"f", "json"},
-                    {"geometryType","esriGeometryPoint"},
-                };
-
-            var Gisfeatures = this.GisApiHelper.GetFeatures< RegionModel>("JNFRegions",0, reqparmForest);
-            if (Gisfeatures.GisAttributes.error == null)
-            {
-                return Ok(Gisfeatures.Features);
-            }
-            else
-            {
-                return StatusCode(500, Gisfeatures.GisAttributes.error.message + " " + Gisfeatures.GisAttributes.error.details[0] + " where:" + reqparmForest.GetValues("where")[0] + " Fields:" + reqparmForest.GetValues("outFields")[0]);
-            }
-        }
-    }
-
-    public class RegionModel
-    {
-        public int? OBJECTID { get; set; }
-        public int merchav_co { get; set; }
-        public int ezor_code { get; set; }
-        public string ezor_name { get; set; }
-    }
-
-    public class GisRegionModel1111 : GisModel
-    {
-        public Features[] features { get; set; }
-        public class Features
-        {
-            public Attributes attributes { get; set; }
-        }
-        public class Attributes : RegionModel
-        {
-        }
-        public class RegionModel
-        {
-            public int? OBJECTID { get; set; }
-            public int merchav_co { get; set; }
-            public int ezor_code { get; set; }
-            public string ezor_name { get; set; }
-        }
-    }
->>>>>>> d1647bf94dd54e593376df04d5bfde8bd46326bf
 }
