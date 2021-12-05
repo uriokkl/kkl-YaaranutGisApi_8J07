084f55cec7e26ebe89236ac31141fc9f4c68302f
u <urio@kkl.org.il>
2021-12-05 09:39:55 +0200

    33333

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace YaaranutGisApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() || env.IsStaging())
+            //if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
